462ac2745949d3b1b82801481f5d2fa6a6abba3d
bluestar <m00157301@gmail.com>
2020-05-07 14:12:09 +0800

    Delete fixed fields.

--- js/animate.js
+++ js/animate.js
@@ -2,32 +2,27 @@
 var i = 0;
 var speed = 1000; // 1 sec
 
-/* container1 */
-var stop_zone = 4
-var expand_container1 = 4
-var divider = "<div><div class='airline_carousel' style='background:white; height:2px'></div></div>"; 
-$(".left_container .airline_carousel:eq(" + (stop_zone-1) + ")").append(divider);
+/* left_container */
+var expand_left_container = 8
 
-$(".left_container .airline_carousel:odd").css("background-color", "#343040");
-$(".left_container .airline_carousel:even").css("background-color", "#657299");
-$(".left_container .airline_carousel:gt(" + stop_zone + ")").css("z-index","-1");
-$(".left_container .airline_carousel").eq(stop_zone).css("background-color", "white");
+$(".left_container .airline_carousel:even").css("background-color", "#343040");
+$(".left_container .airline_carousel:odd").css("background-color", "#657299");
 
-$(".left_container .airline_carousel").eq(expand_container1+2).addClass("expand");
-$(".left_container .airline_carousel").eq(expand_container1+3).addClass("expand");
-$(".left_container .airline_carousel").eq(expand_container1+4).addClass("expand");
-$(".left_container .airline_carousel").eq(expand_container1+5).addClass("expand");
-$(".left_container .airline_carousel").eq(expand_container1+6).addClass("expand");
+$(".left_container .airline_carousel").eq(expand_left_container+1).addClass("expand");
+$(".left_container .airline_carousel").eq(expand_left_container).addClass("expand");
+$(".left_container .airline_carousel").eq(expand_left_container-1).addClass("expand");
+$(".left_container .airline_carousel").eq(expand_left_container-2).addClass("expand");
+$(".left_container .airline_carousel").eq(expand_left_container-3).addClass("expand");
 
 /* right_container */
-var expend_target = 8
+var expand_right_container = 8
 $(".right_container .airline_carousel:even").css("background-color", "#343040");
 $(".right_container .airline_carousel:odd").css("background-color", "#657299");
-$(".right_container .airline_carousel").eq(expend_target+1).addClass("expand")
-$(".right_container .airline_carousel").eq(expend_target).addClass("expand")
-$(".right_container .airline_carousel").eq(expend_target-1).addClass("expand")
-$(".right_container .airline_carousel").eq(expend_target-2).addClass("expand")
-$(".right_container .airline_carousel").eq(expend_target-3).addClass("expand")
+$(".right_container .airline_carousel").eq(expand_right_container+1).addClass("expand")
+$(".right_container .airline_carousel").eq(expand_right_container).addClass("expand")
+$(".right_container .airline_carousel").eq(expand_right_container-1).addClass("expand")
+$(".right_container .airline_carousel").eq(expand_right_container-2).addClass("expand")
+$(".right_container .airline_carousel").eq(expand_right_container-3).addClass("expand")
 
 setTimeout(function () 
 {
@@ -37,35 +32,34 @@ setTimeout(function ()
 //Loop
 time = setInterval(function()
 {
+  var left_container = $(".left_container .airline_carousel");
+  var right_container = $(".right_container .airline_carousel");
+
   if(i%2 == 0) 
   { 
-	  //Container1
-	  var container1 = $(".left_container .airline_carousel:gt(" + stop_zone + ")");
-	  container1.animate({ "top": "-=25px" }, 500, "linear" )
-    container1.eq(expand_container1+2).toggleClass("expand")
-    container1.eq(expand_container1-3).toggleClass("expand")
+	  //left_container
+	  left_container.animate({ "top": "-=25px" }, 500, "linear" )
+    left_container.eq(expand_left_container+2).toggleClass("expand")
+    left_container.eq(expand_left_container-3).toggleClass("expand")
 
-    //Container2
-	  var right_container = $(".right_container .airline_carousel");
+    //right_container
     right_container.animate({ "top": "-=25px" }, 500, "linear" )
-    right_container.eq(expend_target+2).toggleClass("expand")
-    right_container.eq(expend_target-3).toggleClass("expand")
+    right_container.eq(expand_right_container+2).toggleClass("expand")
+    right_container.eq(expand_right_container-3).toggleClass("expand")
   }
   else
   {
-	  //Container1
-	  var container1 = $(".left_container .airline_carousel:gt(" + stop_zone + ")");
-    container1.animate({ "top": "+=25px" }, 0, "linear" )
-    container1.each(function(index)
+	  //left_container
+    left_container.animate({ "top": "+=25px" }, 0, "linear" )
+    left_container.each(function(index)
 	  {
       if(index == 0) 
 	    { 
-        $(this).insertAfter(container1.last()) 
+        $(this).insertAfter(left_container.last()) 
 	    }
     });
 
-    //Container2
-	  var right_container = $(".right_container .airline_carousel");
+    //right_container
     right_container.animate({ "top": "+=25px" }, 0, "linear" )
     right_container.each(function(index)
 	  {
